54b121e625fb10ae2b7403bfaa94131983ea08ff
Bixbyi <qqq0www1@gmail.com>
2020-07-22 18:20:40 +0900

    dsfs

--- pip.js
+++ pip.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const { Cipher } = require('crypto');
 const asnycio = require('asyncio')
 const client = new Discord.Client();
 const token = process.env.token

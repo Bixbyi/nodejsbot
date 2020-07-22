1e6e4717968daa36a7bdd510bda8a857f54cb88f
Bixbyi <qqq0www1@gmail.com>
2020-07-22 18:26:08 +0900

    delete embed

--- pip.js
+++ pip.js
@@ -33,21 +33,4 @@ client.on('message', (message) => {
   )
   }
 });
-if(message.content == 'embed') {
-  let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
-  let embed = new Discord.RichEmbed()
-    .setTitle('타이틀')
-    .setURL('http://www.naver.com')
-    .setAuthor('나긋해', img, 'http://www.naver.com')
-    .setThumbnail(img)
-    .addBlankField()
-    .addField('Inline field title', 'Some value here')
-    .addField('Inline field title', 'Some value here', true)
-    .addField('Inline field title', 'Some value here', true)
-    .addField('Inline field title', 'Some value here', true)
-    .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
-    .addBlankField()
-    .setTimestamp()
-    .setFooter('나긋해')
-}
 client.login(token);
